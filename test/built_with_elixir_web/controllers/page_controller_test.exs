d2ef878c4b0e35b2a85a170c33da6e75888494f6
Edgar Pino <edgarbeltran@me.com>
2018-04-10 16:03:38 -0600

    fix home page test

--- test/built_with_elixir_web/controllers/page_controller_test.exs
+++ test/built_with_elixir_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule BuiltWithElixirWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Built With Elixir - A collection of projects built with Elixir"
   end
 end
