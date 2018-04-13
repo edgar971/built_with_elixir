c0163f4263c8255799356a9108f48cb90b6c20c5
Edgar Pino <edgarbeltran@me.com>
2018-04-12 22:43:22 -0600

    rename posts route to projects

--- lib/built_with_elixir_web/router.ex
+++ lib/built_with_elixir_web/router.ex
@@ -23,6 +23,6 @@ defmodule BuiltWithElixirWeb.Router do
   scope "/api", BuiltWithElixirWeb do
     pipe_through :api
     
-    resources "/posts", PostController, only: [:index, :show]
+    resources "/projects", PostController, only: [:index, :show]
   end
 end
