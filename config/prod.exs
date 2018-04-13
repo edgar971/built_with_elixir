ab3c596bcaee2c65cd9f867927322545138808ae
Edgar Pino <edgarbeltran@me.com>
2018-04-13 11:44:54 -0600

    add heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,16 @@ use Mix.Config
 # which you typically run after static files are built.
 config :built_with_elixir, BuiltWithElixirWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "arcane-anchorage-16238.herokuapp.com", port:443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+# Configure your database
+config :built_with_elixir, BuiltWithElixir.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
