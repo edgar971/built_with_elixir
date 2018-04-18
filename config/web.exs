21a50fb22b1e40c9bc847dcbe12992ee33e546dc
Edgar Pino <edgarbeltran@me.com>
2018-04-18 08:27:05 -0600

    add comment

--- config/web.exs
+++ config/web.exs
@@ -3,4 +3,4 @@ use Mix.Config
 # Config that is put into the front-end window.config
 
 config :built_with_elixir, :web, 
-  google_analytics: Map.get(System.get_env(), "GOOGLE_ANALYTICS_KEY") 
+  google_analytics: Map.get(System.get_env(), "GOOGLE_ANALYTICS_KEY") # Optional. 
