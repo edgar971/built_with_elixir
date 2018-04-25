f98a6589dd33fe675e4d5bc1636576612d2b40b3
Edgar Pino <edgarbeltran@me.com>
2018-04-24 23:48:03 -0600

    add postProject function

--- assets/js/api/index.js
+++ assets/js/api/index.js
@@ -4,3 +4,7 @@ export async function fetchProjects(offset = 0, limit = 10) {
   const { data } = await axios.get('/api/projects', { params: { offset, limit } })
   return data
 }
+
+export async function postProject(project) {
+  return await axios.post('/api/projects', project)
+}
